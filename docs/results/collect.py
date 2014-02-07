f45020a1ad6bed67bd7fff054c7aa1693c96bc1a
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-02-08 00:43:36 +0100

    WHO THE FUCK CAME UP WITH MAKE?

--- docs/results/collect.py
+++ docs/results/collect.py
@@ -1,13 +1,13 @@
 from glob import glob
 
-for result in glob('nursery/unordered/*.txt'):
+for result in glob('mushroom/ascending/*.txt'):
     s = 0.0
     c=0
     with open(result) as f:
         for line in f:
-            if 'real' in line:
+            if 'Finding close sets...' in line:
                 #print line.split('\t')[-1]
-                s += float(line.split('m')[1][:-2])
+                s += float(line.split('\t')[-1][:-2])
                 c += 1
 
         print s / c
